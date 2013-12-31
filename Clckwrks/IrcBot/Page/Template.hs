c777691f571895e740dce2225cec860529069792
Jeremy Shaw <jeremy@seereason.com>
2013-12-31 19:28:59 +0000

    clckwrks-plugin-ircbot-0.6.5: update to work with new theme suport in clckwrks 0.21

--- Clckwrks/IrcBot/Page/Template.hs
+++ Clckwrks/IrcBot/Page/Template.hs
@@ -22,44 +22,10 @@ template :: ( EmbedAsChild IrcBotM headers
          -> IrcBotM Response
 template ttl hdrs bdy =
     do p <- plugins <$> get
-       mTheme <- getTheme p
        (Just clckShowFn) <- getPluginRouteFn p (pluginName clckPlugin)
-       case mTheme of
-         Nothing      -> escape $ internalServerError $ toResponse $ ("No theme package is loaded." :: Text)
-         (Just theme) ->
-             do hdrXml <- fmap (map unClckChild) $ unXMLGenT $ asChild hdrs -- <%> <link rel="stylesheet" type="text/css" href=(IrcBotData "style.css") /> <% hdrs %></%>
-                bdyXml <- fmap (map unClckChild) $ unXMLGenT $ asChild bdy
-                fmap toResponse $ mapClckT f $ ClckT $ withRouteT (\f -> clckShowFn) $ unClckT $ unXMLGenT $ (_themeTemplate theme ttl hdrXml bdyXml)
+       hdrXml <- fmap (map unClckChild) $ unXMLGenT $ asChild hdrs -- <%> <link rel="stylesheet" type="text/css" href=(IrcBotData "style.css") /> <% hdrs %></%>
+       bdyXml <- fmap (map unClckChild) $ unXMLGenT $ asChild bdy
+       fmap toResponse $ mapClckT f $ ClckT $ withRouteT (\f -> clckShowFn) $ unClckT $ (themeTemplate p (ThemeStyleId 0) ttl hdrXml bdyXml)
     where
       f :: ServerPartT IO (a, ClckState) -> ReaderT IrcBotConfig (ServerPartT IO) (a, ClckState)
       f m = ReaderT $ \_ -> m
-
-{-
-
-template :: ( EmbedAsChild IrcBotM headers
-            , EmbedAsChild IrcBotM body
-            ) =>
-            String
-         -> headers
-         -> body
-         -> IrcBotM Response
-template ttl hdrs bdy =
-    do pageTemplate <- ircBotPageTemplate <$> ask
-       fmap toResponse $ unXMLGenT $
-            pageTemplate ttl <%> <link rel="stylesheet" type="text/css" href=(DarcsData "style.css") /> <% hdrs %></%> bdy
--}
-{-
-
-template :: ( EmbedAsChild (Clck ClckURL) headers
-            , EmbedAsChild (Clck ClckURL) body
-            ) =>
-            String
-         -> headers
-         -> body
-         -> IrcBotM Response
-template ttl hdrs bdy =
-    do pageTemplate <- ircBotPageTemplate <$> ask
-       fmap toResponse $ mapClckT lift $ unXMLGenT $
-            pageTemplate ttl hdrs bdy
-
--}
